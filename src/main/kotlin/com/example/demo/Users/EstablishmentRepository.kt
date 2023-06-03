48cf63d912800bb8a8434f1ddddbd038fc771e4a
cdsantosg <73040883+cdsantosg@users.noreply.github.com>
2023-06-02 21:38:11 -0500

    Delete src directory

--- src/main/kotlin/com/example/demo/Users/EstablishmentRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.example.demo.establishments
-
-import org.springframework.data.repository.CrudRepository
-import java.util.UUID
-
-interface EstablishmentRepository : CrudRepository<Establishment, UUID>
